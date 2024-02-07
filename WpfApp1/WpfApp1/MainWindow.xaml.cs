534a4c48e8ac8749404ef2a5bbfd310feec558e9
kei ito <ito.kei@jvckenwood.com>
2024-02-07 11:23:31 +0900

    標準画面実装

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Object> Items { get; } = new ObservableCollection<Object>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for(int i=0; i<200; i++)
+            {
+                Items.Add(new Object());
+            }
+        }
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            Cursor = Cursors.Wait;
+
+            grid.Visibility = Visibility.Visible;
+
+            Dispatcher.InvokeAsync(new Action(() => 
+            {
+                Cursor = null;
+            }), System.Windows.Threading.DispatcherPriority.ApplicationIdle);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new MainWindow2();
+            window.Show();
+
+            this.Close();
+        }
+    }
+}
